b03e11e868d7765c0480ab0d18e4d20bd2e21618
mark.plunkett <markje.p@gmail.com>
2020-12-11 20:32:22 +0000

    Added solution for Day3 part2.

--- Day3/Program.fs
+++ Day3/Program.fs
@@ -1,42 +1,36 @@
 open System
-
-let parseGridToList lines =
-    let rec parseGridRec lines grid =
-        match lines with
-        | [] -> grid
-        | line::remaining -> 
-            let parsedLine = Seq.map (function | '.' -> false | '#' -> true) line
-            parseGridRec remaining (parsedLine::grid)
     
-    parseGridRec lines []
-    |> List.rev
-
-let isTree (grid:string[]) x y =
-    grid.[y].[x] = '#'
-
-let printGrid grid =
-    grid
-    |> List.iter (
-        Seq.map (function | true -> '#' | false -> '.')
-        >> Seq.toArray
-        >> String
-        >> printfn "%s"
-    )
-    
-let solve (grid:string[]) =
+let solvePart1 (grid:string[]) =
     let m = grid.[0].Length
     Seq.initInfinite (fun i -> (i * 3) % m, i)
     |> Seq.takeWhile (fun (_, y) -> y < grid.Length)
-    |> Seq.where (fun (x, y) -> grid.[x].[y] = '#')
+    |> Seq.where (fun (x, y) -> grid.[y].[x] = '#')
     |> Seq.length
 
+let solvePart2 (grid:string[]) =
+    let m = grid.[0].Length
+    [
+        fun i -> i, i
+        fun i -> i * 3, i
+        fun i -> i * 5, i
+        fun i -> i * 7, i
+        fun i -> i, i * 2
+    ]
+    |> Seq.map (
+        Seq.initInfinite
+        >> Seq.takeWhile (fun (_, y) -> y < grid.Length)
+        >> Seq.where (fun (x, y) -> grid.[y].[x % m] = '#')
+        >> Seq.length
+    )
+    |> Seq.reduce (*)
+    
 [<EntryPoint>]
 let main _ =
 
     Seq.initInfinite (fun _ -> Console.ReadLine())
     |> Seq.takeWhile (String.IsNullOrEmpty >> not)
     |> Seq.toArray
-    |> solve
+    |> solvePart2
     |> printfn "%i"
 
     0
