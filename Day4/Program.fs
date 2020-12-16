63e2a406cd81cad62bc801340fa20e401b4f26dc
mark.plunkett <markje.p@gmail.com>
2020-12-16 18:09:03 +0000

    Added partial solution for day4 p2.

--- Day4/Program.fs
+++ Day4/Program.fs
@@ -1,7 +1,8 @@
 open FSharpPlus
 open System
+open System.Collections.Generic
 
-let expected = Set.ofList [
+let expectedFields = Set.ofList [
     "byr"
     "iyr"
     "eyr"
@@ -11,17 +12,35 @@ let expected = Set.ofList [
     "pid"
 ]
 
-let tee f v =
-    f v
-    v
-
-let isValid (passport:string) =
+let isValidPart1 (passport:string) =
     let fields =
         passport.Split(' ')
         |> Seq.map (fun token -> token.Split(':').[0])
         |> Set.ofSeq
 
-    Set.forall (fields.Contains) expected
+    Set.forall (fields.Contains) expectedFields
+
+let areValuesValid (values:IDictionary<string, string>) =
+    let isValueValid key value =
+        match key with
+        | "byr" -> value |> int |> fun i -> i >= 1920 && i <= 2002
+        | "iyr" -> value |> int |> fun i -> i >= 2010 && i <= 2020
+        | "eyr" -> value |> int |> fun i -> i >= 2020 && i <= 2030
+        | "hgt" -> true
+        // Incomplete matches but cba
+
+    values
+    |> Dict.keys
+    |> Seq.forall (fun key -> isValueValid key values.[key]) 
+
+let isValidPart2 (passport:string) =
+    let fields =
+        passport.Split(' ')
+        |> Seq.map (String.split [":"] >> Seq.toList >> fun [a;b] -> a, b)
+        |> dict
+    
+    Set.forall (fields.ContainsKey) expectedFields
+    && areValuesValid fields
 
 let sanitize (s:string) =
     s
@@ -34,8 +53,7 @@ let main argv =
     let input = IO.File.ReadAllText(argv.[0])
     String.split ["\n\n"] input
     |> Seq.map sanitize
-    |> tee (printfn "%A")
-    |> Seq.filter isValid
+    |> Seq.filter isValidPart1
     |> Seq.length
     |> printfn "%i" 
 
