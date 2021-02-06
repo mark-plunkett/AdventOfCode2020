76e29f9cc6fa87e81e6fcdb0bd171b20f49214ae
mark.plunkett <markje.p@gmail.com>
2021-02-06 20:53:47 +0000

    Solved day5 part1.

--- Day5/Program.fs
+++ Day5/Program.fs
@@ -1,23 +1,28 @@
 open System
 
-let stringToInt mapping s =
-    s
-    |> Seq.map mapping
-    |> String.concat String.Empty
-    |> fun i -> Convert.ToInt32(i, 2)
+module Part1 =
+    let stringToInt mapping s =
+        s
+        |> Seq.map mapping
+        |> String.concat String.Empty
+        |> fun i -> Convert.ToInt32(i, 2)
 
-let calculateSeatID (s:string) =
-    let row = s.Substring(0, 7) |> stringToInt (function | 'B' -> "1" | 'F' -> "0") 
-    let col = s.Substring(7, 3) |> stringToInt (function | 'R' -> "1" | 'L' -> "0") 
-    (row * 8) + col
+    let calculateSeatID (s:string) =
+        let row = s.Substring(0, 7) |> stringToInt (function | 'B' -> "1" | 'F' -> "0") 
+        let col = s.Substring(7, 3) |> stringToInt (function | 'R' -> "1" | 'L' -> "0") 
+        (row * 8) + col
+
+    let solve seats =
+        seats
+        |> Seq.map calculateSeatID
+        |> Seq.max
+        |> printfn "%i"
 
 [<EntryPoint>]
 let main _ =
 
     Seq.initInfinite (fun _ -> Console.ReadLine())
     |> Seq.takeWhile (String.IsNullOrEmpty >> not)
-    |> Seq.map calculateSeatID
-    |> Seq.max
-    |> printfn "%i"
+    |> Part1.solve
 
     0
