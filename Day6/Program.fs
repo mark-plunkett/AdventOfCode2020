b57387d35f95ea1145721eaef86ada488e96f8c4
Mark Plunkett <markje.p@gmail.com>
2021-02-08 20:10:22 +0000

    WIP.

--- Day6/Program.fs
+++ Day6/Program.fs
@@ -7,6 +7,27 @@ module Part1 =
         |> Array.map (fun s -> s.Replace("\n", "") |> Seq.distinct |> Seq.length)
         |> Array.reduce (+)
 
+module Part2 =
+    let countCommonAnswers (group:string) =
+        let people = group.Split("\n")
+        people
+        // count the number of letters that are similar between all lines in group
+        |> Array.fold (fun map s ->
+            Map.tryFind s map
+            |> Option.map ((+) 1)
+            |> Option.defaultValue 0
+            |> Map.add s
+        ) Map.empty
+        |> Map.toSeq
+        |> Seq.map snd
+        |> Seq.filter ((=) people.Length)
+        |> Seq.reduce (+)
+
+    let solve (lines:string) =
+        lines.Replace("\r", "").Split("\n\n")
+        |> Array.map countCommonAnswers
+        |> Array.reduce (+)
+
 [<EntryPoint>]
 let main argv =
     let path = argv.[0]
